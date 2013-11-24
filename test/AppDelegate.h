1acbb9b08c178be189e3cb8b4ec69c748b4bf88e
mattjmorrison <mattj.morrison@gmail.com>
2013-11-24 12:29:01 -0600

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by imtapps on 11/24/13.
+//  Copyright (c) 2013 imtapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by imtapps on 11/24/13.
+//  Copyright (c) 2013 imtapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
